da73285741aff3f3ed177e2fb8e3274e73f5a9eb
abdelrahmansadek225 <abdelrahmansadek109@gmail.com>
2024-11-20 15:35:19 +0200

    first commit

---
+++ lib/constraint/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+class Maincolor{
+  static Color primarycolor=const Color(0xFFff4b4b);
+  static Color secondrycolor=const Color(0xFFffca27);
+  static Color accentcolor=const Color(0xFF4169e8);
+}
