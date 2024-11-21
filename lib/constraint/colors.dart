17289b9ade072e444b0c0562d67cd5e06d567e0d
abdelrahmansadek225 <abdelrahmansadek109@gmail.com>
2024-11-21 15:22:29 +0200

    new edit color

--- lib/constraint/colors.dart
+++ lib/constraint/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 class Maincolor{
-  static Color primarycolor=const Color(0xFFff4b4b);
-  static Color secondrycolor=const Color(0xFFffca27);
-  static Color accentcolor=const Color(0xFF4169e8);
+  static Color primarycolor=const Color.fromARGB(255, 29, 3, 73);
+  static Color secondrycolor=const Color.fromARGB(255, 232, 218, 25);
+  static Color accentcolor=const Color.fromARGB(255, 82, 201, 84);
 }
